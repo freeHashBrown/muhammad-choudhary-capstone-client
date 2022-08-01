b8979ebf21e34380d1741091baa1586a2e3ebb73
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-01 18:00:04 -0400

    Create header title, logo and also colors partial

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./Header.scss";
+import logo from "../../assets/logo/oak-tree.png";
 
 const Header = () => {
     return (
-        <header>
-            
+        <header className='header'>
+            <img src={logo} alt="logo" className='header__logo'/>
+            <h1 className='header__title'>
+                Learning Platform
+            </h1>
         </header>
     );
 };
