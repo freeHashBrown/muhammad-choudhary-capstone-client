e2fac59c1786807d1ecb8d91db809e260b74b249
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-02 10:16:29 -0400

    Create homepage and import it into app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.scss';
 import Header from './component/Header/Header';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
    <div>
     <Header/>
     {/* <LoginPage/> */}
-    <SignupPage/>
+    {/* <SignupPage/>*/}
+    <HomePage/>
    </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./HomePage.scss";
+
+const HomePage = () => {
+    return (
+        <main>
+
+        </main>
+    );
+};
+
+export default HomePage;
