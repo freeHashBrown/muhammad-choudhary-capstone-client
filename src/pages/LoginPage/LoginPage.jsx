bddce1b60f7a77d9c2c67b4ac9743f277b8f50a1
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-01 23:00:42 -0400

    Complete sign in form on the right of login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -5,7 +5,7 @@ import googleLogo from "../../assets/icons/google-icon.png";
 const LoginPage = () => {
     return (
         <main className='login-page'>
-            <article className='login-page__column'>
+            <article className='login-page__column login-page__column--select'>
                 <section className='login-page__outer-button'>
                     <img src={googleLogo} alt="google" className='login-page__logo'/>
                     <button className='login-page__inner-button'>
@@ -14,7 +14,10 @@ const LoginPage = () => {
                 </section>
             </article>
             <article className='login-page__column'>
-
+                <form className='login-page__form'>
+                    <input type="text" name="" id="" className='login-page__input' placeholder='username'/>
+                    <input type="password" className='login-page__input' placeholder='password'/>
+                </form>
             </article>
         </main>
     );
