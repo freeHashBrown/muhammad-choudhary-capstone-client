4a341735acc4f261c8d9b237079acdaedd36ab57
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-01 23:11:44 -0400

    Complete login page by adding a background card

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -5,20 +5,22 @@ import googleLogo from "../../assets/icons/google-icon.png";
 const LoginPage = () => {
     return (
         <main className='login-page'>
-            <article className='login-page__column login-page__column--select'>
-                <section className='login-page__outer-button'>
-                    <img src={googleLogo} alt="google" className='login-page__logo'/>
-                    <button className='login-page__inner-button'>
-                        Google
-                    </button>
-                </section>
-            </article>
-            <article className='login-page__column'>
-                <form className='login-page__form'>
-                    <input type="text" name="" id="" className='login-page__input' placeholder='username'/>
-                    <input type="password" className='login-page__input' placeholder='password'/>
-                </form>
-            </article>
+            <div className='login-page__card'>
+                <article className='login-page__column login-page__column--select'>
+                    <section className='login-page__outer-button'>
+                        <img src={googleLogo} alt="google" className='login-page__logo'/>
+                        <button className='login-page__inner-button'>
+                            Google
+                        </button>
+                    </section>
+                </article>
+                <article className='login-page__column'>
+                    <form className='login-page__form'>
+                        <input type="text" name="" id="" className='login-page__input' placeholder='username'/>
+                        <input type="password" className='login-page__input' placeholder='password'/>
+                    </form>
+                </article>
+            </div>
         </main>
     );
 };
