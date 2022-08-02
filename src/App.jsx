7a266e90d1f8fe1b3fe63af940591a0ee3e22cf8
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-02 09:34:07 -0400

    Create signup page and import into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './component/Header/Header';
 import LoginPage from './pages/LoginPage/LoginPage';
+import SignupPage from './pages/SignupPage/SignupPage';
 
 function App() {
   return (
    <div>
     <Header/>
-    <LoginPage/>
+    {/* <LoginPage/> */}
+    <SignupPage/>
    </div>
   );
 }
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./SignupPage.scss";
+
+const SignupPage = () => {
+    return (
+        <main>
+            <article>
+                
+            </article>
+        </main>
+    );
+};
+
+export default SignupPage;
