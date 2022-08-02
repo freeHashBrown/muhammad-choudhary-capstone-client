11cd7c2bdbd6df3bd43c879a67b43b5acfceb9b1
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-02 09:42:25 -0400

    Complete basic layout of sign up page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -3,10 +3,19 @@ import "./SignupPage.scss";
 
 const SignupPage = () => {
     return (
-        <main>
-            <article>
-                
-            </article>
+        <main className='signup-page'>
+            <form className='signup-page__form'>
+                <input type="text" placeholder='First Name' className='sign-up__input'/>
+                <input type="text" placeholder='Last Name' className='sign-up__input'/>
+                <input type="text" placeholder='Username' className='sign-up__input'/>
+                <input type="text" placeholder='Password' className='sign-up__input'/>
+                <input type="text" placeholder='Confirm Password' className='sign-up__input'/>
+                <section className='sign-up__outer-button'>
+                    <button className='sign-up__inner-button'>
+                        Sign Up
+                    </button>
+                </section>
+            </form>
         </main>
     );
 };
