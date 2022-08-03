355ff114464335346f49367fdf26c9b9cff8f6fc
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-03 11:52:39 -0400

    Create comment form and import it into homepage

---
+++ src/component/CommentForm/CommentForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CommentForm = () => {
+    return (
+        <form action="" className='comment-form'>
+            <textarea className='comment-form__input' name="" id=""></textarea>
+            <section className='comment-form__outer-button'>
+                <img className='comment-form__button' src="" alt="" />
+                <p>
+                    Post
+                </p>
+            </section>
+               
+
+         
+        </form>
+    );
+};
+
+export default CommentForm;
