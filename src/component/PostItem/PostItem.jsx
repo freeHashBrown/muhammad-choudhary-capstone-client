ae144d6c2c2beb009343f8f758ea3675e0bf766d
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-04 11:19:49 -0400

    Create postitem and import it into postlist

---
+++ src/component/PostItem/PostItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PostItem = () => {
+    return (
+        <section className='post-item'>
+            <div className='post-item__top'>
+                <p className='post-item__label'>
+                    Muhammad Choudhary
+                </p>
+                <p className='post-item__label'>
+                    2022-08-04
+                </p>
+            </div>
+            <div className='post-item__bottom'>
+                <h1 className='post-item__title'>
+                    This is the first post!
+                </h1>
+                <p className='post-item__desc'>
+                At vero eos et accusamus et iusto odio dignissimos ducimus 
+                qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                quos dolores et quas molestias excepturi sint occaecati 
+                cupiditate non provident, similique sunt in culpa qui officia deserunt 
+                mollitia animi, id est laborum et dolorum fuga. 
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default PostItem;
