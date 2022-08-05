7a8c4f23196025659f3d20cad7ecb1efb9587927
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-05 10:46:29 -0400

    Create form for registering users on the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Header from './component/Header/Header';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
+
   return (
    <div>
-    <Header/>
-    {/* <LoginPage/> */}
-    <SignupPage/>
-    {/* <HomePage/> */}
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={LoginPage}/>
+          <Route path="/signup" component={SignupPage}/>
+          <Route path="/homepage" component={HomePage}/>
+        </Switch>
+      </BrowserRouter>
    </div>
   );
 }
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./SignupPage.scss";
 import signupIcon from "../../assets/icons/signup-icon.png";
 
 const SignupPage = () => {
+
+    //Hooks to store values
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    //Function for submitting username and password
+    const handleSubmit = (event) => {
+        
+
+        const form = event.target;
+
+        //Store the username, password and confirm password
+        const username = form.username.value;
+        const password = form.password.value;
+        const confirmPassword = form.confirmPassword.value;
+
+        // Check all fields filled
+        if (!username || !password || !confirmPassword) {
+        alert("You must fill out all fields");
+        return;
+        }
+  
+        // Check if the passwords match
+        if (password !== confirmPassword) {
+        alert("Passwords must match");
+        return;
+        }
+
+        //Store all the values in hooks
+        
+
+
+
+    }
+
     return (
         <main className='signup-page'>
           
-            <form className='signup-page__form'>
-                {/* <input type="text" placeholder='First Name' className='signup-page__input'/>
-                <input type="text" placeholder='Last Name' className='signup-page__input'/> */}
-                <input type="text" placeholder='Username' className='signup-page__input'/>
-                <input type="text" placeholder='Password' className='signup-page__input'/>
-                <input type="text" placeholder='Confirm Password' className='signup-page__input'/>
+            <form className='signup-page__form' onSubmit={handleSubmit}>
+                <input type="text" placeholder='Username' className='signup-page__input' name='username'/>
+
+                <input type="password" placeholder='Password' className='signup-page__input' name='password'/>
+
+                <input type="password" placeholder='Confirm Password' className='signup-page__input' name='confirmPassword'/>
+
                 <section className='signup-page__outer-button'>
                     <img src={signupIcon} alt="login" className='signup-page__logo '/>
                     <button className='signup-page__inner-button'>
@@ -19,6 +56,7 @@ const SignupPage = () => {
                     </button>
                 </section>
             </form>
+
         </main>
     );
 };
