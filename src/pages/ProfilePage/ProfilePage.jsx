c4bc42f9a33695792d103a78363e5868402eef1c
Muhammad Choudhary <choudha1@sheridancollege.ca>
2022-08-07 17:18:00 -0400

    Create Profile Page and import it into app

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./ProfilePage";
+
+class ProfilePage extends Component {
+    render() {
+        return (
+            <main>
+
+            </main>
+        );
+    }
+}
+
+export default ProfilePage;
